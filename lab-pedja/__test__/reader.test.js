7e8a9556ce2fddffd1585ed474471eb81c4967b7
Predrag Josifovic <pjosifovic@yahoo.com>
2017-11-29 18:54:00 -0800

    adding edge case test for invalid file paths

--- lab-pedja/__test__/reader.test.js
+++ lab-pedja/__test__/reader.test.js
@@ -7,11 +7,22 @@ const paths = [
   `${__dirname}/../assets/file2.txt`,
   `${__dirname}/../assets/file3.txt`];
 
+const invalidPaths = [
+  `${__dirname}/../assets/file1.rtf`,
+  `${__dirname}/../assets/file2.rtf`,
+  `${__dirname}/../assets/file3.rtf`];
+
 describe('reader.test.js', () => {
   test('If not error is found, text from all three files should be shown as an item in an array', (done) => {
     fileReader.readFile(paths, (error,data) => {
       expect(error).toBeNull();
       expect(data).toEqual(['file1\n','file2\n','file3\n']);
+      done();
+    });
+  });
+  test('If invalid file paths are used', (done) => {
+    fileReader.readFile(invalidPaths, (error) => {
+      expect(error).not.toBeNull();
       done();
     });
   });
