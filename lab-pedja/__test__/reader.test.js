41e93e1a0a46ca6bbea256e207f5d0e3646b22e1
Predrag Josifovic <pjosifovic@yahoo.com>
2017-11-29 18:51:30 -0800

    finally passing test - adding text from files to single array as its items

--- lab-pedja/__test__/reader.test.js
+++ lab-pedja/__test__/reader.test.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fileReader = require('../lib/reader');
+
+const paths = [
+  `${__dirname}/../assets/file1.txt`,
+  `${__dirname}/../assets/file2.txt`,
+  `${__dirname}/../assets/file3.txt`];
+
+describe('reader.test.js', () => {
+  test('If not error is found, text from all three files should be shown as an item in an array', (done) => {
+    fileReader.readFile(paths, (error,data) => {
+      expect(error).toBeNull();
+      expect(data).toEqual(['file1\n','file2\n','file3\n']);
+      done();
+    });
+  });
+});
--- lab-pedja/lib/reader.js
+++ lab-pedja/lib/reader.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fileReader = module.exports = {};
+const fs = require('fs');
+
+fileReader.readFile = (paths, callback) => {
+  let returnedArray = [];
+  fs.readFile(paths[0], (error,data) => {
+    if(error)
+      return callback(error);
+    returnedArray.push(data.toString());
+  });
+  fs.readFile(paths[1], (error,data) => {
+    if(error)
+      return callback(error);
+    returnedArray.push(data.toString());
+  });
+  fs.readFile(paths[2], (error,data) => {
+    if(error)
+      return callback(error);
+    returnedArray.push(data.toString());
+    console.log(returnedArray);
+    callback(null,returnedArray);
+  });
+};
